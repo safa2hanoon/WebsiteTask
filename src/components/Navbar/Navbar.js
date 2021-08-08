10e0cbb80793b71e1b8580f3087633431a3155bd
safa2hanoon <safahanoon@gmail.com>
2021-08-08 11:10:18 +0300

    deploy react app to github Pages

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "../Button/Button";
+
 import "./styles.css";
 import "../../App.css";
 
